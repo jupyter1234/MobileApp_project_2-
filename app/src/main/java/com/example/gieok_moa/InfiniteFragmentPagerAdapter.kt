0486e30de9950c6d52c691db1941a775578f42ed
bokyeong <wntjdals0412@gmail.com>
2023-11-25 22:42:43 +0900

    [feat] : 페이지 이동시 현재 달, 이전 달, 다음 달, 년도 변화 페이지 인덱스를 기반으로 월 증가

--- app/src/main/java/com/example/gieok_moa/InfiniteFragmentPagerAdapter.kt
+++ app/src/main/java/com/example/gieok_moa/InfiniteFragmentPagerAdapter.kt
@@ -2,16 +2,31 @@ package com.example.gieok_moa
 
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentActivity
+import androidx.recyclerview.widget.RecyclerView.ViewHolder
 import androidx.viewpager2.adapter.FragmentStateAdapter
+import androidx.viewpager2.adapter.FragmentViewHolder
+import com.example.gieok_moa.databinding.FragmentInfiniteBinding
 
-class InfiniteFragmentPagerAdapter(fragmentActivity: FragmentActivity) : FragmentStateAdapter(fragmentActivity){
-    private val pageCount = Int.MAX_VALUE
+//main <- infiniteFragment를 설정하여 씌우는 adapter
+
+class InfiniteFragmentPagerAdapter(activity: FragmentActivity) : FragmentStateAdapter(activity) {
+    //override fun ViewHolder(val binding: FragmentInfiniteBinding) :
+
     val FragmentPosition = Int.MAX_VALUE/2
-    override fun getItemCount(): Int = Int.MAX_VALUE
 
+    override fun getItemCount(): Int = Int.MAX_VALUE
     override fun createFragment(position: Int): Fragment {
-        val calendarFragment = CalendarFragment()
-        calendarFragment.pageIndex = position
-        return calendarFragment
+        val infiniteFragment = InfiniteFragment()
+        infiniteFragment.pageIndex = position
+        return infiniteFragment
+    }
+    //각 페이지마다 현재 년도, 현재 달, 다음 달, 이전 달 설정해주기
+    override fun onBindViewHolder(
+        holder: FragmentViewHolder,
+        position: Int,
+        payloads: MutableList<Any>
+    ) {
+
+        super.onBindViewHolder(holder, position, payloads)
     }
 }
--- app/src/main/java/com/example/gieok_moa/MainActivity.kt
+++ app/src/main/java/com/example/gieok_moa/MainActivity.kt
@@ -2,6 +2,8 @@ package com.example.gieok_moa
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import androidx.core.content.ContextCompat
 import androidx.viewpager2.widget.ViewPager2
 import com.example.gieok_moa.databinding.ActivityMainBinding
 
@@ -11,6 +13,16 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
 
+        //toolbar 뒤로가기 버튼
+        var back = ContextCompat.getDrawable(this, R.drawable.arrow)
+        setSupportActionBar(binding.maintoolbar)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        supportActionBar?.setHomeAsUpIndicator(back)
+        title = ""
+        binding.viewpager.adapter = InfiniteFragmentPagerAdapter(this)
+        binding.viewpager.orientation = ViewPager2.ORIENTATION_VERTICAL
+
+        //어댑터 적용
         binding.viewpager.adapter = InfiniteFragmentPagerAdapter(this)
         binding.viewpager.orientation = ViewPager2.ORIENTATION_VERTICAL
     }
