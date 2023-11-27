7d87944f5f37689815237ddc4c89ac78ca8d1ea3
gobomin <0311bom@naver.com>
2023-11-27 16:30:19 +0900

    add : added some requisite code

--- app/src/main/java/com/example/gieok_moa/MainFragment.kt
+++ app/src/main/java/com/example/gieok_moa/MainFragment.kt
@@ -1,5 +1,8 @@
 package com.example.gieok_moa
 
+import android.app.AlertDialog
+import android.app.ProgressDialog.show
+import android.content.Intent
 import android.os.Bundle
 import android.util.Log
 import androidx.fragment.app.Fragment
@@ -10,6 +13,7 @@ import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.gieok_moa.databinding.FragmentMainBinding
 import com.example.gieok_moa.databinding.ItemSnapBinding
+import com.example.gieok_moa.databinding.SelectionDialogBinding
 import java.util.Date
 import java.text.SimpleDateFormat
 class MainFragment : Fragment() {
@@ -24,11 +28,14 @@ class MainFragment : Fragment() {
         binding.date.text = currentDate
 
         binding.settingButton.setOnClickListener {
-            Log.d("check", "setting button clicked")
+            //Log.d("check", "setting button clicked")
+//            val intent: Intent = Intent(this, oooActivity::class.java)
+//            startActivity(intent)
         }
-
         binding.calendarButton.setOnClickListener {
-            Log.d("check", "calendar button clicked")
+            //Log.d("check", "calendar button clicked")
+            //val intent: Intent = Intent(this, oooActivity::class.java)
+            //startActivity(intent)
         }
 
         val datas: MutableList<Snap> = mutableListOf()
@@ -37,6 +44,8 @@ class MainFragment : Fragment() {
         datas.add(snapEx)
         datas.add(snapAddButton)
 
+        // 데이터 정렬 함수 추가
+
 //        datas.add(snapEx)
 //        datas.add(snapEx)
 //        datas.add(snapEx)
@@ -44,17 +53,32 @@ class MainFragment : Fragment() {
 
         val layoutManager = GridLayoutManager(activity, 2)
         binding.recyclerView.layoutManager = layoutManager
+
         val adapter = MyAdapter(datas){ snap ->
             //Log.d("check", "snap clicked")
             if (snap.image == R.drawable.snap_add_button){
                 // when snap add button clicked -> popup & move to snap add page
 
+                // selection dialog
+                val selectionDialog = SelectionDialogBinding.inflate(layoutInflater)
+                AlertDialog.Builder(getActivity()).run {
+                    setView(selectionDialog.root)
+                    selectionDialog.takePic.setOnClickListener {
+                        // move to Camera App
+
+                    }
+                    selectionDialog.fromGal.setOnClickListener {
+                        // move to Gallery App
+                    }
+                    show()
+                }
             }
             else{
                 // when snap clicked -> dialog
                 
             }
         }
+
         binding.recyclerView.adapter = adapter
 
         return binding.root
