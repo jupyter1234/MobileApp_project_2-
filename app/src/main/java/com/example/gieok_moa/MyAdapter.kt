f9710dd212ea1279ca64551cb4c3f8a16019b699
gobomin <0311bom@naver.com>
2023-12-21 18:39:57 +0900

    sub merge

--- app/src/main/java/com/example/gieok_moa/MyAdapter.kt
+++ app/src/main/java/com/example/gieok_moa/MyAdapter.kt
@@ -4,6 +4,7 @@ import android.util.Log
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.core.content.ContentProviderCompat.requireContext
+import androidx.core.content.ContextCompat
 import androidx.core.graphics.drawable.toDrawable
 import androidx.core.net.toUri
 import androidx.recyclerview.widget.RecyclerView
@@ -19,7 +20,7 @@ class MyAdapter(val datas: MutableList<Snap>, val clickListener: (Snap)->Unit): 
     private lateinit var binding: ItemSnapBinding
     inner class MyViewHolder(val binding: ItemSnapBinding) : RecyclerView.ViewHolder(binding.root){
         fun bind(item: Snap){
-            if (item.photoUrl != "android.resource://com.example.gieok_moa/drawable/snap_add_button1") {
+            if (item.photoUrl != "android.resource://com.example.gieok_moa/drawable/snap_add_button") {
                 binding.snapTime.text = SimpleDateFormat("HH:mm").format(item.createdDate)
             }
             Glide.with(binding.root)
@@ -35,7 +36,7 @@ class MyAdapter(val datas: MutableList<Snap>, val clickListener: (Snap)->Unit): 
         val item = datas[position]
         (holder as MyViewHolder).bind(item)
 
-        if (item.photoUrl != "android.resource://com.example.gieok_moa/drawable/snap_add_button1"){
+        if (item.photoUrl != "android.resource://com.example.gieok_moa/drawable/snap_add_button"){
             Log.d("ko", "니뭔데")
             val binding = (holder as MyViewHolder).binding
             val db = UserDatabase.getInstance(holder.itemView.context)
@@ -45,9 +46,9 @@ class MyAdapter(val datas: MutableList<Snap>, val clickListener: (Snap)->Unit): 
                     if (tag.ownedSnapID == item.snapId){
                         binding.tag.text = tag.staus
                         binding.tag.background = when (tag.color) {
-                            Color.RED -> R.drawable.red_blur_background.toDrawable()
-                            Color.GREEN -> R.drawable.green_blur_background.toDrawable()
-                            Color.YELLOW -> R.drawable.yellow_blur_background.toDrawable()
+                            Color.RED -> ContextCompat.getDrawable(binding.root.context, R.drawable.red_blur_background)
+                            Color.GREEN -> ContextCompat.getDrawable(binding.root.context, R.drawable.green_blur_background)
+                            Color.YELLOW -> ContextCompat.getDrawable(binding.root.context, R.drawable.yellow_blur_background)
                         }
                     }
                 }
